67961940ce046dfa2486700fe609241bf346a3d2
Dominic <dtorres243@west-mec.org>
2020-01-08 18:24:31 -0700

    vibin pt2

--- tny_script.js
+++ tny_script.js
@@ -10,30 +10,55 @@
    Date: 1/6/20
 
 */
+runClock();
+setInterval("runClock()", 1000)
 
-document.getElementById("dateNow").innerHTML="m/d/y<br/>h:m:s";
+function runClock(){
 
 //created a variable for today's date
-var date = new Date("January 7, 2020 4:46:39");
+var currentYear = new Date();
 //changed today's date
-var thisDate = date.toLocaleDateString(); //thisDate == 1/7/20
-var thisTime = date.toLocaleTimeString(); //thisTime == 4:46:39 PM
+var thisDate = currentYear.toLocaleDateString(); //thisDate == 1/7/20
+var thisTime = currentYear.toLocaleTimeString(); //thisTime == 4:46:39 PM
 
 document.getElementById("dateNow").innerHTML = thisDate + "<br/>" + thisTime;
 
+//calculate the days until Jan 1st
+var newYear = new Date("January 1, 2020");
+var nextYear = currentYear.getFullYear()+1;
+newYear.setFullYear(nextYear);
+var daysLeft = (newYear - currentYear)/(1000*60*60*24);
+var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+var minLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+var secLeft = (minLeft - Math.floor(minLeft))*60;
 //changed the countdown clock
-document.getElementById("days").innerHTML = "DD";
-document.getElementById("hrs").innerHTML = "HH";
-document.getElementById("mins").innerHTML = "MM";
-document.getElementById("secs").innerHTML = "SS";
+document.getElementById("days").innerHTML = Math.floor(daysLeft);
+document.getElementById("hrs").innerHTML = Math.floor(hrsLeft);
+document.getElementById("mins").innerHTML = Math.floor(minLeft);
+document.getElementById("secs").innerHTML = Math.ceil(secLeft);
+}
 
-var daysTill = 365 - date.getDate() -1;
-var hoursTill = 24 - date.getHours() -1;
-var minTill = 60 - date.getMinutes() -1;
-var secTill = 60 - date.getSeconds();
 
-document.getElementById("days").innerHTML = daysTill;
-document.getElementById("hrs").innerHTML = hoursTill;
-document.getElementById("mins").innerHTML = minTill;
-document.getElementById("secs").innerHTML = secTill;
+function rectArea(length, width){
+   var area = length * width;
+   return area;
+}
+var rect = rectArea(5, 2);
+console.log(rect);
+console.log(rectArea(200, 40));
 
+
+
+function circArea(radius){
+   var area = Math.PI*Math.pow(radius, 2)
+   return area;
+}
+console.log(circArea(10));
+
+
+function distance(x1, y1, x2, y2){
+   var dist = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y1-y2, 2));
+   return dist;
+}
+
+console.log(distance(2,3,5,7));//5
